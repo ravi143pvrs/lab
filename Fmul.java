089c5dd1b8a327bc3878e80bec692475b884bdeb
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-06-24 11:35:46 +0530

    Update Fmul.java

--- Fmul.java
+++ Fmul.java
@@ -1,15 +1,19 @@
 // @author ravi kumar
 
+/* multiplication of floating numbers programe */
 
 public class Fmul {
 
+	
 	public static void main(String[] args) {
+
+		// assigning values
 		
 	float a = 45.43F;
 	float b = 28.56F;
 	float c = a*b;
 	
-
+// multiplication process and giving the output by compiler
 		System.out.println("Answer for multiplication of numbers:-  "+c);
 		
 	}
